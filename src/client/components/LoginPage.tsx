b9e39bbaf98967318b49447c80bb0e3da8f81270
thinkty <the.thinkty@gmail.com>
2022-08-20 10:06:33 -0400

    Using react-use-script to load scripts dynamically

--- src/client/components/LoginPage.tsx
+++ src/client/components/LoginPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';
+import useScript from 'react-script-hook';
 import { useAuth } from './AuthProvider';
 
 /**
@@ -55,22 +56,26 @@ export const LoginPage = ({
       });
   }
 
-  React.useEffect(() => {
-    google.accounts.id.initialize({
-      client_id: '950484081367-5u480vk65qg38kogmka1ptaqpacrhpnj.apps.googleusercontent.com',
-      auto_select: false,
-      context: 'signin',
-      callback: handleGoogleLogin,
-    });
-    google.accounts.id.renderButton(
-      document.getElementById('googleAuthButton')!,
-      {theme: 'filled_blue', size: 'medium', type: 'standard', width: '300px' }
-    );
-  }, []);
+  // Load Google Sign In script
+  const [ gsiScriptLoading, gsiScriptError ] = useScript({
+    src: 'https://accounts.google.com/gsi/client',
+    onload: () => {
+      google.accounts.id.initialize({
+        client_id: '950484081367-5u480vk65qg38kogmka1ptaqpacrhpnj.apps.googleusercontent.com',
+        auto_select: false,
+        context: 'signin',
+        callback: handleGoogleLogin,
+      });
+      google.accounts.id.renderButton(
+        document.getElementById('googleAuthButton')!,
+        {theme: 'filled_blue', size: 'medium', type: 'standard', width: '300px' }
+      );
+    },
+  });
 
-  // TODO: update UI
-  return (
-    <>
+  if (gsiScriptError) {
+    console.error(gsiScriptError);
+    return (
       <div
         style={{
           width: '100vw',
@@ -81,10 +86,28 @@ export const LoginPage = ({
           alignItems: 'center',
         }}
       >
-        <h1>
-          Login
-        </h1>
+        Error
+      </div>
+    );
+  }
 
+  return (
+    <>
+      <div
+        style={{
+          width: '100vw',
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: 10,
+        }}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="32" width="32">
+          <path xmlns="http://www.w3.org/2000/svg" d="M14 19C13.4477 19 13 19.4477 13 20C13 20.5523 13.4477 21 14 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3H14C13.4477 3 13 3.44771 13 4C13 4.55228 13.4477 5 14 5H19V19H14Z" fill="#ffffff"></path>
+          <path xmlns="http://www.w3.org/2000/svg" d="M15.7136 12.7005C15.8063 12.6062 15.8764 12.498 15.9241 12.3828C15.9727 12.2657 15.9996 12.1375 16 12.003L16 12L16 11.997C15.9992 11.7421 15.9016 11.4874 15.7071 11.2929L11.7071 7.29289C11.3166 6.90237 10.6834 6.90237 10.2929 7.29289C9.90237 7.68342 9.90237 8.31658 10.2929 8.70711L12.5858 11H3C2.44771 11 2 11.4477 2 12C2 12.5523 2.44771 13 3 13H12.5858L10.2929 15.2929C9.90237 15.6834 9.90237 16.3166 10.2929 16.7071C10.6834 17.0976 11.3166 17.0976 11.7071 16.7071L15.7064 12.7078L15.7136 12.7005Z" fill="#ffffff"></path>
+        </svg>
         <div
           style={{
             border: 'thin solid white',
@@ -99,7 +122,10 @@ export const LoginPage = ({
         >
           <div
             id='googleAuthButton'
-            style={{ width: '100%' }}
+            style={{ width: '100%', display: gsiScriptLoading ? 'none' : 'block' }}
+          />
+          <div
+
           />
         </div>
       </div>
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,7 @@
 import express from 'express';
 import { FirebaseOptions, initializeApp } from 'firebase/app';
-import { randomBytes } from 'crypto';
 import * as helmet from 'helmet';
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-import { readFile } from 'fs';
-import path from 'path';
 import { authRouter } from './auth';
 
 dotenv.config();
@@ -22,22 +19,11 @@ initializeApp(firebaseConfig);
 
 const app = express();
 
-// Middleware for setting nonce on each request. !! NONCE IS NOT IMPLEMENTED !!
-// @see https://github.com/helmetjs/helmet#reference
-// TODO: add logging
-app.use((_, res, next) => {
-  res.locals.nonce = randomBytes(16).toString('hex');
-  next();
-});
-
 // Setting CSP to allow google sign-in scripts
 // @see https://developers.google.com/identity/gsi/web/guides/get-google-api-clientid#content_security_policy
-// For adding nonce in the script-src, casting the http Response type to an Express Response type shows locals
-// @see https://stackoverflow.com/a/71721914/12448426
 app.use(helmet.contentSecurityPolicy({
   directives: {
-    // scriptSrc: [`'self'`, `'unsafe-inline'`, 'https://accounts.google.com/gsi/client'],
-    scriptSrc: [`'self'`, `'unsafe-inline'`, (_, res) => `'nonce-${(res as express.Response).locals.nonce}'`],
+    scriptSrc: [`'self'`, `'unsafe-inline'`, 'https://accounts.google.com/gsi/client'],
     connectSrc: [`'self'`, 'https://accounts.google.com/gsi/'],
     frameSrc: [`'self'`, 'https://accounts.google.com/gsi/'],
     styleSrc: [`'self'`, `'unsafe-inline'`, 'https://accounts.google.com/gsi/style'],
@@ -50,25 +36,6 @@ app.use(helmet.crossOriginOpenerPolicy({ policy: 'same-origin-allow-popups' }));
 app.use(express.json());
 app.use(express.urlencoded( {extended : false } ));
 app.use(authRouter);
-
-// Injecting nonce to external scripts
-app.get('/', (_, res) => {
-  readFile(path.resolve(__dirname, "../../public/index.html"), "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send("An error occurred");
-    }
-
-    // Google Sign In
-    data = data.replace(
-      '<script src="https://accounts.google.com/gsi/client" async defer></script>',
-      `<script src="https://accounts.google.com/gsi/client" nonce="${res.locals.nonce}" async defer></script>`
-    );
-
-    return res.send(data);
-  });
-});
-
 app.use(express.static('public'));
 
 // TODO: check jwt before doing any operation
