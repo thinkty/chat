5db76fb4f029185cbec68c074bee909653a41001
thinkty <the.thinkty@gmail.com>
2022-08-20 09:41:22 -0400

    Added nonce

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,11 +3,12 @@ import { FirebaseOptions, initializeApp } from 'firebase/app';
 import { randomBytes } from 'crypto';
 import * as helmet from 'helmet';
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import { readFile } from 'fs';
+import path from 'path';
 import { authRouter } from './auth';
 
 dotenv.config();
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -15,7 +16,6 @@ const firebaseConfig: FirebaseOptions = {
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
   databaseURL: `https://${process.env.FIREBASE_DB_NAME}.firebaseio.com`,
 };
 initializeApp(firebaseConfig);
@@ -36,8 +36,8 @@ app.use((_, res, next) => {
 // @see https://stackoverflow.com/a/71721914/12448426
 app.use(helmet.contentSecurityPolicy({
   directives: {
-    scriptSrc: [`'self'`, `'unsafe-inline'`, 'https://accounts.google.com/gsi/client'],
-    // scriptSrc: [`'self'`, `'unsafe-inline'`, 'https://accounts.google.com/gsi/client', (req, res) => `'nonce-${(res as express.Response).locals.nonce}'`],
+    // scriptSrc: [`'self'`, `'unsafe-inline'`, 'https://accounts.google.com/gsi/client'],
+    scriptSrc: [`'self'`, `'unsafe-inline'`, (_, res) => `'nonce-${(res as express.Response).locals.nonce}'`],
     connectSrc: [`'self'`, 'https://accounts.google.com/gsi/'],
     frameSrc: [`'self'`, 'https://accounts.google.com/gsi/'],
     styleSrc: [`'self'`, `'unsafe-inline'`, 'https://accounts.google.com/gsi/style'],
@@ -50,22 +50,29 @@ app.use(helmet.crossOriginOpenerPolicy({ policy: 'same-origin-allow-popups' }));
 app.use(express.json());
 app.use(express.urlencoded( {extended : false } ));
 app.use(authRouter);
+
+// Injecting nonce to external scripts
+app.get('/', (_, res) => {
+  readFile(path.resolve(__dirname, "../../public/index.html"), "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("An error occurred");
+    }
+
+    // Google Sign In
+    data = data.replace(
+      '<script src="https://accounts.google.com/gsi/client" async defer></script>',
+      `<script src="https://accounts.google.com/gsi/client" nonce="${res.locals.nonce}" async defer></script>`
+    );
+
+    return res.send(data);
+  });
+});
+
 app.use(express.static('public'));
 
-// app.get('*bundle.js', (req, res) => {
-//   res.sendFile('bundle.js', { root: path.join(__dirname, '../../public') });
-// });
-// app.get('*bundle.js.map', (req, res) => {
-//   res.sendFile('bundle.map.js', { root: path.join(__dirname, '../../public') });
-// });
-
-// // Setting a wildcard due to react-router paths
-// app.get('*', (req, res) => {
-//   res.sendFile('index.html', { root: path.join(__dirname, '../../public') });
-// });
-
-// TODO: check jwt (or idToken for Google) before doing any operation
-app.get('/api', (req, res) => {
+// TODO: check jwt before doing any operation
+app.get('/api', (_, res) => {
   res.send('hello world');
 });
 
